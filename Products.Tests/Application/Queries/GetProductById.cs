25e7194d8a2b50416433a086a76070647b89288e
Vladesko <borodich01@list.ru>
2025-06-17 14:08:47 +0300

    AddSuccessTestForQueries

---
+++ Products.Tests/Application/Queries/GetProductById.cs
@@ -0,0 +1,44 @@
+﻿using Application.Products.Queries.GetProductById;
+using Domain.Abstractions;
+using Domain.Product;
+using Moq;
+using Shouldly;
+
+namespace Products.Tests.Application.Queries
+{
+    public class GetProductById
+    {
+        private const int DEFAULT_ID_1 = 1;
+
+        private const string DEFAULT_NAME_1 = "Name1";
+        private const string DEFAULT_NAME_2 = "Name2";
+
+        private const decimal DEFAULT_PRICE_1 = 1;
+        private const decimal DEFAULT_PRICE_2 = 2;
+
+        private readonly Mock<IProductRepository> _mockProductRepository;
+        public GetProductById()
+        {
+            _mockProductRepository = new Mock<IProductRepository>();
+        }
+        [Fact]
+        public async Task Handle_Should_ResultSuccess_IdIsExist()
+        {
+            //Ararnge
+             var product = Product.Create(DEFAULT_NAME_1, DEFAULT_PRICE_2);
+            _mockProductRepository.
+                Setup(r => r.GetByIdAsync(DEFAULT_ID_1, It.IsAny<CancellationToken>())).
+                ReturnsAsync(product);
+
+            var query = new GetProductByIdQuery(DEFAULT_ID_1);
+            var handler = new GetProductByIdQueryHandler(_mockProductRepository.Object);
+            //Act
+
+            Result<Product> result = await handler.Handle(query, It.IsAny<CancellationToken>());
+
+            //Assert
+            result.IsSuccess.ShouldBeTrue();
+            result.Value.ShouldBe(product);
+        }
+    }
+}
---
+++ Products.Tests/Application/Queries/GetProductsTests.cs
@@ -0,0 +1,48 @@
+﻿using Application.Products.Queries.GetAll;
+using Domain.Abstractions;
+using Domain.Product;
+using Moq;
+using Shouldly;
+
+namespace Products.Tests.Application.Queries
+{
+    public class GetProductsTests
+    {
+        private const string DEFAULT_NAME_1 = "Name1";
+        private const string DEFAULT_NAME_2 = "Name2";
+
+        private const decimal DEFAULT_PRICE_1 = 1;
+        private const decimal DEFAULT_PRICE_2 = 2;
+
+        private readonly Mock<IProductRepository> _mockProductRepository;
+        public GetProductsTests()
+        {
+            _mockProductRepository = new Mock<IProductRepository>();
+        }
+        [Fact]
+        public async Task Handle_Should_ResultSuccess()
+        {
+            //Arrange
+            IEnumerable<Product> products = new Product[] //Necessary to indicate that this is an array
+                {
+                    Product.Create(DEFAULT_NAME_1, DEFAULT_PRICE_1),
+                    Product.Create(DEFAULT_NAME_2, DEFAULT_PRICE_2)
+                };
+
+            _mockProductRepository.
+                Setup(r => r.GetAllAsync(It.IsAny<CancellationToken>())).
+                ReturnsAsync(products);
+
+            var qeury = new GetAllProductQuery();
+            var handler = new GetAllProductQueryHandler(_mockProductRepository.Object);
+
+            //Act
+            Result<IEnumerable<Product>> result = await handler.
+                Handle(qeury, It.IsAny<CancellationToken>());
+
+            //Assert
+            result.IsSuccess.ShouldBeTrue();
+            result.Value.ShouldBe(products);            
+        }
+    }
+}
